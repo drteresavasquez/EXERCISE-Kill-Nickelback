7d4a65ff64a103de020d5e1e264f125bbfe28349
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-10-05 15:37:34 -0500

    first commit

---
+++ nickelback.rb
@@ -0,0 +1,16 @@
+songs = [
+    ['Nickelback', 'How You Remind Me'],
+    ['Will.i.am', 'That Power'],
+    ['Miles Davis', 'Stella by Starlight'],
+    ['Nickelback', 'Animals']
+]
+
+another_array = []
+
+for band in songs
+    if band[0] != "Nickelback"
+        another_array << band
+    end
+end
+
+puts another_array
